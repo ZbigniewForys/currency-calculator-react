175bdbae05f6e9ab8b25c791d6cf9a68b7f0af70
Zbigniew Forys <zbigniew.forys@wp.pl>
2023-05-07 17:05:02 +0200

    Version to check

--- src/Table/Tr/index.js
+++ src/Table/Tr/index.js
@@ -1,20 +1,25 @@
 import "./style.css";
 const Tr = ({ currencyTable }) =>
-(
+( 
     <tr>
-        <th className="table__cell table__header">{currencyTable.currency}</th>
-        <td className="table__cell"><input
+             <th className="table__cell table__header">{currencyTable.currency}</th>
+        <td className="table__cell">
+            <input
             className="table__inputCell"
             value={currencyTable.rate}
             type="number" step="0.0001"
-            readonly></input >
+            readOnly={true}
+            ></input >
         </td>
-        <td className="table__cell"><input className="table__inputCell"
+        <td className="table__cell"><input 
+        className="table__inputCell"
             type="number" step="0.01"
-            readonly></input>
+            value={currencyTable.value}
+            readOnly={true}
+            ></input>
         </td>
-    </tr >
-);
+    </tr>
+ );
 
 export default Tr;
 
